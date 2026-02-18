e0efc5019e35b43e1812dd47f46a884352eda1db
dhruvbishnoid-coder <dhruvbishnoid@gmail.com>
2026-02-19 01:45:37 +0530

    main.py

--- main.py
+++ main.py
@@ -10,10 +10,21 @@ def shorten_link(url):
     response = requests.get(api_url)
     return response.text
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.reply_to(
+        message,
+        "Welcome 😊\n\nFollow me on Instagram 👇\nhttps://www.instagram.com/dhruvxbeniwal/"
+    )
+
 @bot.message_handler(func=lambda message: True)
 def send_short_link(message):
     long_url = message.text
-    short_url = shorten_link(long_url)
-    bot.reply_to(message, f"Short Link: {short_url}")
+    
+    if long_url.startswith("http"):
+        short_url = shorten_link(long_url)
+        bot.reply_to(message, f"Short Link: {short_url}")
+    else:
+        bot.reply_to(message, "Please send a valid link starting with http or https")
 
 bot.polling()
